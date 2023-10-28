54a85bf9176a9dcd788daaee7aaeef6d10a809f0
isabellagoes <isabellagoes.igs@gmail.com>
2023-10-27 21:40:39 -0300

    Sequência de números determinada por um inicial e um limite

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿Console.Clear();
+
+int numero1;
+int numero2;
+int aux;
+
+do
+{
+    Console.Write("Primeiro número: ");
+    Int32.TryParse(Console.ReadLine()!, out numero1);
+
+    Console.Write("último número: ");
+    Int32.TryParse(Console.ReadLine()!, out numero2);    
+}
+while(numero1 > numero2);
+
+aux = numero1;
+Console.WriteLine();
+
+while(aux <= numero2)
+{
+    Console.Write($"{aux} ");
+    aux++;
+}
+
+Console.WriteLine();
+Console.WriteLine($"Esta foi a sequência de números de {numero1} a {numero2}");
+Console.WriteLine();
